f9bb9d126ba899f6827e12ec2fb0b1e20438c161
lichuanqi <1016126757@qq.com>
2020-07-21 00:22:51 +0800

    'xxx'

--- src/containers/AppDataShow.jsx
+++ src/containers/AppDataShow.jsx
@@ -3,44 +3,51 @@ import PropTypes from 'prop-types'
 import { Layout, Row, Col } from 'antd'
 
 const AppDataShow = props => {
-  let { dataShow } = props
-  return (
-    <Layout className='data-show animated fadeIn'>
-      <Row gutter={24} className='index-header'>
-          <Col span={6}>
-              <div className='base-style'>
-                  <p>今日完成数</p>
-                  <p>{dataShow.todayFinish}</p>
-                  <div className='line'></div>
-              </div>
-          </Col>
-          <Col span={6}>
-              <div className='base-style'>
-                  <p>今日待完成数</p>
-                  <p>{dataShow.todayUnFinish}</p>
-                  <div className='line'></div>
-              </div>
-          </Col>
-          <Col span={6}>
-              <div className='base-style'>
-                  <p>今日总数</p>
-                  <p>{dataShow.todayTotal}</p>
-                  <div className='line'></div>
-              </div>
-          </Col>
-          <Col span={6}>
-              <div className='base-style'>
-                  <p>累计完成数</p>
-                  <p>{dataShow.total}</p>
-              </div>
-          </Col>
-      </Row>
-    </Layout>
-  )
+    let { dataShow } = props
+    return (
+        <Layout className='data-show animated fadeIn'>
+            <Row gutter={20} className='index-header'>
+                <Col span={4}>
+                    <div className='base-style'>
+                        <p>今日完成数</p>
+                        <p>{dataShow.todayFinish}</p>
+                        <div className='line'></div>
+                    </div>
+                </Col>
+                <Col span={4}>
+                    <div className='base-style'>
+                        <p>今日待完成数</p>
+                        <p>{dataShow.todayUnFinish}</p>
+                        <div className='line'></div>
+                    </div>
+                </Col>
+                <Col span={4}>
+                    <div className='base-style'>
+                        <p>今日总数</p>
+                        <p>{dataShow.todayTotal}</p>
+                        <div className='line'></div>
+                    </div>
+                </Col>
+                <Col span={4}>
+                    <div className='base-style'>
+                        <p>当月完成数</p>
+                        <p>{dataShow.monthTotal}</p>
+                        <div className='line'></div>
+                    </div>
+                </Col>
+                <Col span={4}>
+                    <div className='base-style'>
+                        <p>累计完成数</p>
+                        <p>{dataShow.total}</p>
+                    </div>
+                </Col>
+            </Row>
+        </Layout>
+    )
 }
 
 AppDataShow.propTypes = {
-  dataShow: PropTypes.object,
+    dataShow: PropTypes.object
 }
 
 export default AppDataShow
